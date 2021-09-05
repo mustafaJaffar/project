d95001f27ad7e7ade79c81700302f2ad541d132e
mustafaJaffar <83813982+mustafaJaffar@users.noreply.github.com>
2021-09-05 16:37:03 +0100

    Add files via upload

---
+++ cp file.py
@@ -0,0 +1,55 @@
+import pdb
+
+a = 5
+pdb.set_trace()
+b = 10
+
+c = 6
+
+answer1 = a + b * c
+
+print ("answer1 =", answer1)
+
+answer2 = (a + b) * c
+
+print ("answer2 =", answer2)
+
+# two
+
+a = input('enter 1: ')
+b = input('enter 2 : ')
+c = input('enter 3: ')
+
+d = input('enter unpaintable 1: ')
+e = input('enter unpaintable 2: ')
+
+print( 'total paint required is', (2*a*b + 2*a*c + b*c - (d*e)) // 11)
+
+# three
+
+lastMilage <- USERINPUT
+milage <- USERINPUT
+litres <- USERINPUT
+
+gallon <- litres * 0.22
+
+OUTPUT  ( milage - lastMilage ) / gallons
+
+constant = gallon per litre
+integer = milage
+real = miles per gallon
+
+# four
+
+books <- USERINPUT
+kids <- USERINPUT
+
+booksPerKid <- books // kids
+overflow <- books % kids
+
+OUTPUT booksPerKid
+OUTPUT overflow
+
+
+
+
